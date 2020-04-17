ff072db21bb7a02fcca50d9b75e34536dbbabf6b
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-04-17 14:45:07 +0100

    First commit.

---
+++ src/test/java/com/chisimdi/restfulwebservices/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisimdi.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
